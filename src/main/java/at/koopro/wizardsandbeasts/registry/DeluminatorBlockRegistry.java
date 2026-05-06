3958a306a8cb3bb34f9789dcc3813ee4e56af185
Koopro <christian.schallner@icloud.com>
2026-05-06 20:59:31 +0200

    squash(chore/mod-blocks-registry-split): group block registrations
    Split ModBlocks into wizarding-world and deluminator grouped registries while preserving ModBlocks public constants for compatibility and cleaner navigation.

---
+++ src/main/java/at/koopro/wizardsandbeasts/registry/DeluminatorBlockRegistry.java
@@ -0,0 +1,95 @@
+package at.koopro.wizardsandbeasts.registry;
+
+import at.koopro.wizardsandbeasts.block.DeluminatorLightBlock;
+import at.koopro.wizardsandbeasts.block.UnlitTorchBlock;
+import at.koopro.wizardsandbeasts.block.UnlitWallTorchBlock;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LanternBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.PushReaction;
+import net.neoforged.neoforge.registries.DeferredBlock;
+
+final class DeluminatorBlockRegistry {
+    static final DeferredBlock<DeluminatorLightBlock> DELUMINATOR_LIGHT =
+            ModBlocks.BLOCKS.registerBlock("deluminator_light", DeluminatorLightBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .noCollision()
+                            .noOcclusion()
+                            .replaceable()
+                            .instabreak()
+                            .lightLevel(s -> 15)
+                            .pushReaction(PushReaction.DESTROY));
+
+    static final DeferredBlock<UnlitTorchBlock> UNLIT_TORCH =
+            ModBlocks.BLOCKS.registerBlock("unlit_torch",
+                    props -> new UnlitTorchBlock(ParticleTypes.FLAME, props),
+                    BlockBehaviour.Properties.of()
+                            .noCollision()
+                            .instabreak()
+                            .sound(SoundType.WOOD));
+    static final DeferredBlock<UnlitWallTorchBlock> UNLIT_WALL_TORCH =
+            ModBlocks.BLOCKS.registerBlock("unlit_wall_torch",
+                    props -> new UnlitWallTorchBlock(ParticleTypes.FLAME, props),
+                    BlockBehaviour.Properties.of()
+                            .noCollision()
+                            .instabreak()
+                            .sound(SoundType.WOOD));
+    static final DeferredBlock<UnlitTorchBlock> UNLIT_SOUL_TORCH =
+            ModBlocks.BLOCKS.registerBlock("unlit_soul_torch",
+                    props -> new UnlitTorchBlock(ParticleTypes.SOUL_FIRE_FLAME, props),
+                    BlockBehaviour.Properties.of()
+                            .noCollision()
+                            .instabreak()
+                            .sound(SoundType.WOOD));
+    static final DeferredBlock<UnlitWallTorchBlock> UNLIT_SOUL_WALL_TORCH =
+            ModBlocks.BLOCKS.registerBlock("unlit_soul_wall_torch",
+                    props -> new UnlitWallTorchBlock(ParticleTypes.SOUL_FIRE_FLAME, props),
+                    BlockBehaviour.Properties.of()
+                            .noCollision()
+                            .instabreak()
+                            .sound(SoundType.WOOD));
+    static final DeferredBlock<UnlitTorchBlock> UNLIT_COPPER_TORCH =
+            ModBlocks.BLOCKS.registerBlock("unlit_copper_torch",
+                    props -> new UnlitTorchBlock(ParticleTypes.FLAME, props),
+                    BlockBehaviour.Properties.of()
+                            .noCollision()
+                            .instabreak()
+                            .sound(SoundType.WOOD));
+    static final DeferredBlock<UnlitWallTorchBlock> UNLIT_COPPER_WALL_TORCH =
+            ModBlocks.BLOCKS.registerBlock("unlit_copper_wall_torch",
+                    props -> new UnlitWallTorchBlock(ParticleTypes.FLAME, props),
+                    BlockBehaviour.Properties.of()
+                            .noCollision()
+                            .instabreak()
+                            .sound(SoundType.WOOD));
+
+    static final DeferredBlock<LanternBlock> UNLIT_LANTERN =
+            ModBlocks.BLOCKS.registerBlock("unlit_lantern", LanternBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .requiresCorrectToolForDrops()
+                            .strength(3.5F)
+                            .sound(SoundType.LANTERN));
+    static final DeferredBlock<LanternBlock> UNLIT_SOUL_LANTERN =
+            ModBlocks.BLOCKS.registerBlock("unlit_soul_lantern", LanternBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .requiresCorrectToolForDrops()
+                            .strength(3.5F)
+                            .sound(SoundType.LANTERN));
+    static final DeferredBlock<LanternBlock> UNLIT_COPPER_LANTERN =
+            ModBlocks.BLOCKS.registerBlock("unlit_copper_lantern", LanternBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .requiresCorrectToolForDrops()
+                            .strength(3.5F)
+                            .sound(SoundType.LANTERN));
+
+    static final DeferredBlock<Block> UNLIT_GLOWSTONE =
+            ModBlocks.BLOCKS.registerBlock("unlit_glowstone", Block::new,
+                    BlockBehaviour.Properties.of()
+                            .strength(0.3F)
+                            .sound(SoundType.GLASS));
+
+    private DeluminatorBlockRegistry() {
+    }
+}
--- src/main/java/at/koopro/wizardsandbeasts/registry/ModBlocks.java
+++ src/main/java/at/koopro/wizardsandbeasts/registry/ModBlocks.java
@@ -1,25 +1,13 @@
 package at.koopro.wizardsandbeasts.registry;
 
 import at.koopro.wizardsandbeasts.WizardsAndBeastsMod;
-import at.koopro.wizardsandbeasts.block.DeluminatorLightBlock;
-import at.koopro.wizardsandbeasts.block.FloatingCandleBlock;
-import at.koopro.wizardsandbeasts.block.HouseBannerBlock;
-import at.koopro.wizardsandbeasts.block.MallowsweetBlock;
 import at.koopro.wizardsandbeasts.block.MandrakeCropBlock;
-import at.koopro.wizardsandbeasts.block.SpellTeacherBlock;
-import at.koopro.wizardsandbeasts.block.UnlitTorchBlock;
-import at.koopro.wizardsandbeasts.block.UnlitWallTorchBlock;
-import at.koopro.wizardsandbeasts.wand.block.WandmakersBenchBlock;
 import at.koopro.wizardsandbeasts.world.ModConfiguredFeatures;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.LanternBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -104,178 +92,47 @@ public class ModBlocks {
 
     // --- Wizarding world ---
 
-    public static final DeferredBlock<Block> DEVILS_SNARE =
-            BLOCKS.registerBlock("devils_snare", Block::new, BlockBehaviour.Properties.ofFullCopy(Blocks.COBWEB));
+    public static final DeferredBlock<Block> DEVILS_SNARE = WizardingWorldBlockRegistry.DEVILS_SNARE;
+    public static final DeferredBlock<? extends Block> MALLOWSWEET = WizardingWorldBlockRegistry.MALLOWSWEET;
+    public static final DeferredBlock<? extends Block> GRYFFINDOR_BANNER = WizardingWorldBlockRegistry.GRYFFINDOR_BANNER;
+    public static final DeferredBlock<? extends Block> SLYTHERIN_BANNER = WizardingWorldBlockRegistry.SLYTHERIN_BANNER;
+    public static final DeferredBlock<? extends Block> RAVENCLAW_BANNER = WizardingWorldBlockRegistry.RAVENCLAW_BANNER;
+    public static final DeferredBlock<? extends Block> HUFFLEPUFF_BANNER = WizardingWorldBlockRegistry.HUFFLEPUFF_BANNER;
+    public static final DeferredBlock<? extends Block> FLOATING_CANDLE = WizardingWorldBlockRegistry.FLOATING_CANDLE;
+    public static final DeferredBlock<Block> BRASS_CAULDRON = WizardingWorldBlockRegistry.BRASS_CAULDRON;
+    public static final DeferredBlock<Block> WIZARDING_COPPER_CAULDRON = WizardingWorldBlockRegistry.WIZARDING_COPPER_CAULDRON;
+    public static final DeferredBlock<Block> PEWTER_CAULDRON = WizardingWorldBlockRegistry.PEWTER_CAULDRON;
+    public static final DeferredBlock<Block> FLOO_GRATE = WizardingWorldBlockRegistry.FLOO_GRATE;
+    public static final DeferredBlock<? extends Block> SPELL_TEACHER = WizardingWorldBlockRegistry.SPELL_TEACHER;
+    public static final DeferredBlock<? extends Block> WANDMAKERS_BENCH = WizardingWorldBlockRegistry.WANDMAKERS_BENCH;
 
-    public static final DeferredBlock<MallowsweetBlock> MALLOWSWEET =
-            BLOCKS.registerBlock("mallowsweet", MallowsweetBlock::new,
-                    BlockBehaviour.Properties.of()
-                            .mapColor(MapColor.PLANT)
-                            .noCollision()
-                            .instabreak()
-                            .sound(SoundType.GRASS)
-                            .offsetType(BlockBehaviour.OffsetType.XZ)
-                            .pushReaction(PushReaction.DESTROY));
-
-    public static final DeferredBlock<HouseBannerBlock> GRYFFINDOR_BANNER =
-            BLOCKS.registerBlock("gryffindor_banner", HouseBannerBlock::new,
-                    BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_RED).instabreak().sound(SoundType.WOOL).noOcclusion());
-    public static final DeferredBlock<HouseBannerBlock> SLYTHERIN_BANNER =
-            BLOCKS.registerBlock("slytherin_banner", HouseBannerBlock::new,
-                    BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN).instabreak().sound(SoundType.WOOL).noOcclusion());
-    public static final DeferredBlock<HouseBannerBlock> RAVENCLAW_BANNER =
-            BLOCKS.registerBlock("ravenclaw_banner", HouseBannerBlock::new,
-                    BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLUE).instabreak().sound(SoundType.WOOL).noOcclusion());
-    public static final DeferredBlock<HouseBannerBlock> HUFFLEPUFF_BANNER =
-            BLOCKS.registerBlock("hufflepuff_banner", HouseBannerBlock::new,
-                    BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).instabreak().sound(SoundType.WOOL).noOcclusion());
-
-    public static final DeferredBlock<FloatingCandleBlock> FLOATING_CANDLE =
-            BLOCKS.registerBlock("floating_candle", FloatingCandleBlock::new,
-                    BlockBehaviour.Properties.of()
-                            .mapColor(MapColor.WOOL)
-                            .noCollision()
-                            .instabreak()
-                            .lightLevel(s -> 14)
-                            .sound(SoundType.CANDLE)
-                            .noOcclusion());
-
-    public static final DeferredBlock<Block> BRASS_CAULDRON =
-            BLOCKS.registerBlock("brass_cauldron", Block::new,
-                    BlockBehaviour.Properties.of().strength(2.0f).sound(SoundType.COPPER).noOcclusion());
-    public static final DeferredBlock<Block> WIZARDING_COPPER_CAULDRON =
-            BLOCKS.registerBlock("wizarding_copper_cauldron", Block::new,
-                    BlockBehaviour.Properties.of().strength(2.0f).sound(SoundType.COPPER).noOcclusion());
-    public static final DeferredBlock<Block> PEWTER_CAULDRON =
-            BLOCKS.registerBlock("pewter_cauldron", Block::new,
-                    BlockBehaviour.Properties.of().strength(2.0f).sound(SoundType.COPPER).noOcclusion());
-
-    public static final DeferredBlock<Block> FLOO_GRATE =
-            BLOCKS.registerBlock("floo_grate", Block::new,
-                    BlockBehaviour.Properties.of().strength(3.0f).sound(SoundType.COPPER).noOcclusion());
-
-    public static final DeferredBlock<SpellTeacherBlock> SPELL_TEACHER =
-            BLOCKS.registerBlock("spell_teacher", SpellTeacherBlock::new,
-                    BlockBehaviour.Properties.of().strength(2.5f).sound(SoundType.WOOD));
-
-    public static final DeferredBlock<WandmakersBenchBlock> WANDMAKERS_BENCH =
-            BLOCKS.registerBlock("wandmakers_bench", WandmakersBenchBlock::new,
-                    BlockBehaviour.Properties.of().strength(2.5f).sound(SoundType.WOOD));
-
-    /** Block items for wizarding blocks (required for loot/datagen: {@code dropSelf} uses {@link Block#asItem()}). */
-    public static final DeferredItem<BlockItem> DEVILS_SNARE_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("devils_snare", DEVILS_SNARE);
-    public static final DeferredItem<BlockItem> MALLOWSWEET_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("mallowsweet", MALLOWSWEET);
-    public static final DeferredItem<BlockItem> GRYFFINDOR_BANNER_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("gryffindor_banner", GRYFFINDOR_BANNER);
-    public static final DeferredItem<BlockItem> SLYTHERIN_BANNER_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("slytherin_banner", SLYTHERIN_BANNER);
-    public static final DeferredItem<BlockItem> RAVENCLAW_BANNER_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("ravenclaw_banner", RAVENCLAW_BANNER);
-    public static final DeferredItem<BlockItem> HUFFLEPUFF_BANNER_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("hufflepuff_banner", HUFFLEPUFF_BANNER);
-    public static final DeferredItem<BlockItem> FLOATING_CANDLE_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("floating_candle", FLOATING_CANDLE);
-    public static final DeferredItem<BlockItem> BRASS_CAULDRON_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("brass_cauldron", BRASS_CAULDRON);
-    public static final DeferredItem<BlockItem> WIZARDING_COPPER_CAULDRON_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("wizarding_copper_cauldron", WIZARDING_COPPER_CAULDRON);
-    public static final DeferredItem<BlockItem> PEWTER_CAULDRON_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("pewter_cauldron", PEWTER_CAULDRON);
-    public static final DeferredItem<BlockItem> FLOO_GRATE_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("floo_grate", FLOO_GRATE);
-    public static final DeferredItem<BlockItem> SPELL_TEACHER_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("spell_teacher", SPELL_TEACHER);
-    public static final DeferredItem<BlockItem> WANDMAKERS_BENCH_ITEM =
-            ModItems.ITEMS.registerSimpleBlockItem("wandmakers_bench", WANDMAKERS_BENCH);
+    public static final DeferredItem<BlockItem> DEVILS_SNARE_ITEM = WizardingWorldBlockRegistry.DEVILS_SNARE_ITEM;
+    public static final DeferredItem<BlockItem> MALLOWSWEET_ITEM = WizardingWorldBlockRegistry.MALLOWSWEET_ITEM;
+    public static final DeferredItem<BlockItem> GRYFFINDOR_BANNER_ITEM = WizardingWorldBlockRegistry.GRYFFINDOR_BANNER_ITEM;
+    public static final DeferredItem<BlockItem> SLYTHERIN_BANNER_ITEM = WizardingWorldBlockRegistry.SLYTHERIN_BANNER_ITEM;
+    public static final DeferredItem<BlockItem> RAVENCLAW_BANNER_ITEM = WizardingWorldBlockRegistry.RAVENCLAW_BANNER_ITEM;
+    public static final DeferredItem<BlockItem> HUFFLEPUFF_BANNER_ITEM = WizardingWorldBlockRegistry.HUFFLEPUFF_BANNER_ITEM;
+    public static final DeferredItem<BlockItem> FLOATING_CANDLE_ITEM = WizardingWorldBlockRegistry.FLOATING_CANDLE_ITEM;
+    public static final DeferredItem<BlockItem> BRASS_CAULDRON_ITEM = WizardingWorldBlockRegistry.BRASS_CAULDRON_ITEM;
+    public static final DeferredItem<BlockItem> WIZARDING_COPPER_CAULDRON_ITEM = WizardingWorldBlockRegistry.WIZARDING_COPPER_CAULDRON_ITEM;
+    public static final DeferredItem<BlockItem> PEWTER_CAULDRON_ITEM = WizardingWorldBlockRegistry.PEWTER_CAULDRON_ITEM;
+    public static final DeferredItem<BlockItem> FLOO_GRATE_ITEM = WizardingWorldBlockRegistry.FLOO_GRATE_ITEM;
+    public static final DeferredItem<BlockItem> SPELL_TEACHER_ITEM = WizardingWorldBlockRegistry.SPELL_TEACHER_ITEM;
+    public static final DeferredItem<BlockItem> WANDMAKERS_BENCH_ITEM = WizardingWorldBlockRegistry.WANDMAKERS_BENCH_ITEM;
 
     // --- Deluminator blocks (no block items — placed/removed only by the Deluminator) ---
 
-    public static final DeferredBlock<DeluminatorLightBlock> DELUMINATOR_LIGHT =
-            BLOCKS.registerBlock("deluminator_light", DeluminatorLightBlock::new,
-                    BlockBehaviour.Properties.of()
-                            .noCollision()
-                            .noOcclusion()
-                            .replaceable()
-                            .instabreak()
-                            .lightLevel(s -> 15)
-                            .pushReaction(PushReaction.DESTROY));
-
-    public static final DeferredBlock<UnlitTorchBlock> UNLIT_TORCH =
-            BLOCKS.registerBlock("unlit_torch",
-                    props -> new UnlitTorchBlock(ParticleTypes.FLAME, props),
-                    BlockBehaviour.Properties.of()
-                            .noCollision()
-                            .instabreak()
-                            .sound(SoundType.WOOD));
-
-    public static final DeferredBlock<UnlitWallTorchBlock> UNLIT_WALL_TORCH =
-            BLOCKS.registerBlock("unlit_wall_torch",
-                    props -> new UnlitWallTorchBlock(ParticleTypes.FLAME, props),
-                    BlockBehaviour.Properties.of()
-                            .noCollision()
-                            .instabreak()
-                            .sound(SoundType.WOOD));
-
-    public static final DeferredBlock<UnlitTorchBlock> UNLIT_SOUL_TORCH =
-            BLOCKS.registerBlock("unlit_soul_torch",
-                    props -> new UnlitTorchBlock(ParticleTypes.SOUL_FIRE_FLAME, props),
-                    BlockBehaviour.Properties.of()
-                            .noCollision()
-                            .instabreak()
-                            .sound(SoundType.WOOD));
-
-    public static final DeferredBlock<UnlitWallTorchBlock> UNLIT_SOUL_WALL_TORCH =
-            BLOCKS.registerBlock("unlit_soul_wall_torch",
-                    props -> new UnlitWallTorchBlock(ParticleTypes.SOUL_FIRE_FLAME, props),
-                    BlockBehaviour.Properties.of()
-                            .noCollision()
-                            .instabreak()
-                            .sound(SoundType.WOOD));
-
-    public static final DeferredBlock<UnlitTorchBlock> UNLIT_COPPER_TORCH =
-            BLOCKS.registerBlock("unlit_copper_torch",
-                    props -> new UnlitTorchBlock(ParticleTypes.FLAME, props),
-                    BlockBehaviour.Properties.of()
-                            .noCollision()
-                            .instabreak()
-                            .sound(SoundType.WOOD));
-
-    public static final DeferredBlock<UnlitWallTorchBlock> UNLIT_COPPER_WALL_TORCH =
-            BLOCKS.registerBlock("unlit_copper_wall_torch",
-                    props -> new UnlitWallTorchBlock(ParticleTypes.FLAME, props),
-                    BlockBehaviour.Properties.of()
-                            .noCollision()
-                            .instabreak()
-                            .sound(SoundType.WOOD));
-
-    public static final DeferredBlock<LanternBlock> UNLIT_LANTERN =
-            BLOCKS.registerBlock("unlit_lantern", LanternBlock::new,
-                    BlockBehaviour.Properties.of()
-                            .requiresCorrectToolForDrops()
-                            .strength(3.5F)
-                            .sound(SoundType.LANTERN));
-
-    public static final DeferredBlock<LanternBlock> UNLIT_SOUL_LANTERN =
-            BLOCKS.registerBlock("unlit_soul_lantern", LanternBlock::new,
-                    BlockBehaviour.Properties.of()
-                            .requiresCorrectToolForDrops()
-                            .strength(3.5F)
-                            .sound(SoundType.LANTERN));
-
-    public static final DeferredBlock<LanternBlock> UNLIT_COPPER_LANTERN =
-            BLOCKS.registerBlock("unlit_copper_lantern", LanternBlock::new,
-                    BlockBehaviour.Properties.of()
-                            .requiresCorrectToolForDrops()
-                            .strength(3.5F)
-                            .sound(SoundType.LANTERN));
-
-    public static final DeferredBlock<Block> UNLIT_GLOWSTONE =
-            BLOCKS.registerBlock("unlit_glowstone", Block::new,
-                    BlockBehaviour.Properties.of()
-                            .strength(0.3F)
-                            .sound(SoundType.GLASS));
+    public static final DeferredBlock<? extends Block> DELUMINATOR_LIGHT = DeluminatorBlockRegistry.DELUMINATOR_LIGHT;
+    public static final DeferredBlock<? extends Block> UNLIT_TORCH = DeluminatorBlockRegistry.UNLIT_TORCH;
+    public static final DeferredBlock<? extends Block> UNLIT_WALL_TORCH = DeluminatorBlockRegistry.UNLIT_WALL_TORCH;
+    public static final DeferredBlock<? extends Block> UNLIT_SOUL_TORCH = DeluminatorBlockRegistry.UNLIT_SOUL_TORCH;
+    public static final DeferredBlock<? extends Block> UNLIT_SOUL_WALL_TORCH = DeluminatorBlockRegistry.UNLIT_SOUL_WALL_TORCH;
+    public static final DeferredBlock<? extends Block> UNLIT_COPPER_TORCH = DeluminatorBlockRegistry.UNLIT_COPPER_TORCH;
+    public static final DeferredBlock<? extends Block> UNLIT_COPPER_WALL_TORCH = DeluminatorBlockRegistry.UNLIT_COPPER_WALL_TORCH;
+    public static final DeferredBlock<? extends Block> UNLIT_LANTERN = DeluminatorBlockRegistry.UNLIT_LANTERN;
+    public static final DeferredBlock<? extends Block> UNLIT_SOUL_LANTERN = DeluminatorBlockRegistry.UNLIT_SOUL_LANTERN;
+    public static final DeferredBlock<? extends Block> UNLIT_COPPER_LANTERN = DeluminatorBlockRegistry.UNLIT_COPPER_LANTERN;
+    public static final DeferredBlock<Block> UNLIT_GLOWSTONE = DeluminatorBlockRegistry.UNLIT_GLOWSTONE;
 
     public static final WoodSet[] ALL_WOOD_SETS = { ELDER, YEW, HOLLY, ROWAN };
 }
